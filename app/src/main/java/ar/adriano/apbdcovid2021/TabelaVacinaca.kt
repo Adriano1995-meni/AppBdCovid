3fa2950e968144b1912cc5f20196c4b80b7115b3
Adrianocorreia-025 <82658411+Adrianocorreia-025@users.noreply.github.com>
2021-06-23 19:23:31 +0100

    Criada a funcao CRUD para as Tabelas de Base de Dados

--- app/src/main/java/ar/adriano/apbdcovid2021/TabelaVacinaca.kt
+++ app/src/main/java/ar/adriano/apbdcovid2021/TabelaVacinaca.kt
@@ -1,5 +1,7 @@
 package ar.adriano.apbdcovid2021
 
+import android.content.ContentValues
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
 
@@ -8,20 +10,46 @@ class TabelaVacinaca (db: SQLiteDatabase) {
 
     fun cria() {
         db.execSQL(
-            "CREATE TABLE $NOME_TABLE(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "CREATE TABLE $NOME_TABELA(${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
                     "$NOME_VACINA TEXT NOT NULL," +
                     "$DATA_DO_SEGUNDA_DOCE DATE NOT NULL," +
                     "$DATA_VALIDACAO DATE NOT NULL," +
                     "$CAMPO_ID_DESTRITO INTEGER NOT NULL," +
                     "$IDPaciente INTEGER NOT NULL," +
                     "FOREIGN KEY($CAMPO_ID_DESTRITO) REFERENCES ${TabelaDestrito.NOME_TABELA}," +
-                    "FOREIGN KEY($IDPaciente) REFERENCES ${TabelaPessoas.NOME_TABELA})"
-        )
+                    "FOREIGN KEY($IDPaciente) REFERENCES ${TabelaPessoas.NOME_TABELA})")
+
+
     }
 
 
+    fun insert(values: ContentValues): Long {
+        return db.insert(NOME_TABELA, null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_TABELA, whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String,
+        selectionArgs: Array<String>,
+        groupBy: String,
+        having: String,
+        orderBy: String
+    ): Cursor? {
+        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
+
+
+
     companion object {
-        const val NOME_TABLE = "Vacina"
+        const val NOME_TABELA = "Vacina"
         const val NOME_VACINA = "nome_Vacina"
         const val DATA_DO_SEGUNDA_DOCE = "DATA_DO_TESTE"
         const val DATA_VALIDACAO ="Data de Validaçao"
